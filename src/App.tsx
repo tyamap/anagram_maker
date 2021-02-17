27041354ab3d6408253ff94f0c1ef0e464cc6d36
tyamap <tom333my@outlook.com>
2021-02-17 21:16:57 +0900

    入力の受け付けとスタイル適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import React, { useEffect, useState } from "react";
 import Anagram from "./components/Anagram";
+import InputForm from "./components/InputForm";
 
+export type Query = {
+  word: string
+  number: number
+}
 function App() {
+  const [query, setQuery] = useState<Query>({word: "", number: 0})
+
+  useEffect(() => {
+    console.log(query.word);
+  }, [query])
   return (
     <div className="App">
-      <Anagram word="あこぎなしょうばい" length={3} />
+      <h1>アナグラムつくるくん</h1>
+      <InputForm setQuery={setQuery}/>
+      <Anagram word={query.word} number={query.number} />
     </div>
   );
 }
