9238bfc240e08b2e6b9fd4b8ab0535a811a8c669
tyamap <tom333my@outlook.com>
2021-03-13 21:14:54 +0900

    入力欄改善

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -7,21 +7,26 @@ const InputForm: React.FC<InputFormProps> = (props) => {
   return (
     <div>
       <div>
-        <input
-          type="text"
-          name="w"
-          ref={props.form.register}
-          required
-          style={{ width: 250 }}
-        />
+        <label>
+          もととなる文字列：
+          <div>
+            <input
+              type="text"
+              name="w"
+              ref={props.form.register}
+              required
+              style={{ width: 250 }}
+            />
+          </div>
+        </label>
       </div>
       <div>
         <label>
-          つくる単語の文字数
+          つくる単語の文字数：
           <input
             type="number"
             name="mn"
-            defaultValue={2}
+            defaultValue={3}
             ref={props.form.register}
             min={2}
             max={15}
