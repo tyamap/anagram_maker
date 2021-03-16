53a4317e822eafd3720f9408903c424968258b51
tyamap <tom333my@outlook.com>
2021-03-16 22:11:07 +0900

    オプションフォームをおしゃれに

--- src/components/OptionsForm.tsx
+++ src/components/OptionsForm.tsx
@@ -1,117 +1,78 @@
+import { kMaxLength } from "buffer";
 import React from "react";
 
 type OptionsFormProps = {
   form: any;
 };
 
+const checkboxes = [
+  { label: "名詞", key: "n" },
+  { label: "動詞", key: "v" },
+  { label: "形容詞", key: "aj" },
+  { label: "副詞", key: "av" },
+  { label: "その他", key: "o" },
+];
+const radios = [
+  { label: "動詞", key: "vob" },
+  { label: "形容詞", key: "ajob" },
+  { label: "副詞", key: "avob" },
+];
 const OptionsForm: React.FC<OptionsFormProps> = (props) => {
   return (
-    <div>
-      <div>
-        対象：
-        <label>
-          <input
-            type="checkbox"
-            name="t"
-            ref={props.form.register}
-            value="n"
-            defaultChecked
-          />
-          名詞
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            name="t"
-            ref={props.form.register}
-            value="v"
-            defaultChecked
-          />
-          動詞
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            name="t"
-            ref={props.form.register}
-            value="aj"
-            defaultChecked
-          />
-          形容詞
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            name="t"
-            ref={props.form.register}
-            value="av"
-            defaultChecked
-          />
-          副詞
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            name="t"
-            ref={props.form.register}
-            value="o"
-            defaultChecked
-          />
-          その他
-        </label>
-      </div>
-      <div>
-        動詞：
-        <label>
-          <input
-            type="radio"
-            name="vob"
-            ref={props.form.register}
-            value={1}
-            defaultChecked
-          />
-          基本形のみ
-        </label>
-        <label>
-          <input type="radio" name="vob" ref={props.form.register} value={0} />
-          活用形も含む
-        </label>
-      </div>
-      <div>
-        形容詞：
-        <label>
-          <input
-            type="radio"
-            name="ajob"
-            ref={props.form.register}
-            value={1}
-            defaultChecked
-          />
-          基本形のみ
-        </label>
-        <label>
-          <input type="radio" name="ajob" ref={props.form.register} value={0} />
-          活用形も含む
-        </label>
-      </div>
-      <div>
-        副詞：
-        <label>
-          <input
-            type="radio"
-            name="avob"
-            ref={props.form.register}
-            value={1}
-            defaultChecked
-          />
-          基本形のみ
-        </label>
-        <label>
-          <input type="radio" name="avob" ref={props.form.register} value={0} />
-          活用形も含む
-        </label>
-      </div>
-    </div>
+    <table>
+      <tr>
+        <td>対象：</td>
+        <td>
+          <div>
+            {checkboxes.map((d) => (
+              <>
+                <input
+                  id={d.key}
+                  type="checkbox"
+                  name="t"
+                  ref={props.form.register}
+                  value={d.key}
+                  defaultChecked
+                />
+                <label htmlFor={d.key} className="checkbox">
+                  {d.label}
+                </label>
+              </>
+            ))}
+          </div>
+        </td>
+      </tr>
+      {radios.map((d) => (
+        <tr>
+          <td>
+            <span>{d.label}：</span>
+          </td>
+          <td>
+            <input
+              id={`${d.key}-1`}
+              type="radio"
+              name={d.key}
+              ref={props.form.register}
+              value={1}
+              defaultChecked
+            />
+            <label htmlFor={`${d.key}-1`} className="radio">
+              基本形のみ
+            </label>
+            <input
+              id={`${d.key}-0`}
+              type="radio"
+              name={d.key}
+              ref={props.form.register}
+              value={0}
+            />
+            <label htmlFor={`${d.key}-0`} className="radio">
+              活用形も含む
+            </label>
+          </td>
+        </tr>
+      ))}
+    </table>
   );
 };
 
